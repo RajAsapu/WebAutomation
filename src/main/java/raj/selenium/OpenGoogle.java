dc351dd69eccce520e0a272566d6d27c14c89a0c
Raj <rjsekhar007@gmail.com>
2018-03-21 22:18:33 -0400

    Added identifiers

--- src/main/java/raj/selenium/OpenGoogle.java
+++ src/main/java/raj/selenium/OpenGoogle.java
@@ -1,13 +1,31 @@
 package raj.selenium;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class OpenGoogle {
     public static void main(String[] args)throws Exception{
-        System.setProperty("webdriver.chrome.driver","./chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\asapu\\OneDrive\\Pictures\\Test Automation\\WebAutomation\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("http://www.google.com");
+        /*
+         * Identifiers(8)
+         * ---------------------
+         * name
+         * className
+         * id
+         * linkText
+         * partialLinkText
+         *
+         * xpath
+         * cssSelector
+         * tagName
+         */
+        driver.findElement(By.className("gsfi")).sendKeys("Minnesota");
+        driver.findElement(By.className("gsfi")).submit();
+        driver.findElement(By.partialLinkText("Wikipedia")).click();
+        driver.findElement(By.linkText("state")).click();
         Thread.sleep(10000);
         driver.close();
     }
